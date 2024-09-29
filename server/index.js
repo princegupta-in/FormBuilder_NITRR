f78304eaaf8047013cdae8714077c8c8f608874a
Utkarshtiwari-1 <tiwariutkarsh571@gmail.com>
2024-09-29 23:24:28 +0530

    added index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const dbconnect = require("./src/db/Connectdb");
+
 const userrouter = require("./src/routes/userRroute");
+
+
 const app = express();
 
 
 dbconnect();
+
 app.use(express.json());
+
+
+
+
 app.use("/api/v1",userrouter);
 app.listen(4000,()=>{
     console.log("app is listning");
